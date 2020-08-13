49c8dde870c34f447f306337e78749ddc9b8bbf1
arun varghese <arun26cs@gmail.com>
2020-08-13 17:42:42 -0400

    changes corrected for myapp

--- src/com/canalogies/initial/test/MyApp.java
+++ src/com/canalogies/initial/test/MyApp.java
@@ -6,12 +6,12 @@ public class MyApp {
 	
 	public static void main(String[] args) {
 		
-		 
-		
-		Coach coach = new BasketBallCaoch();
-		System.out.println(coach.getAdvise());
-		
-		coach = new TrackCoach();
-		System.out.println(coach.getAdvise());
+//		 
+//		
+//		Coach coach = new BasketBallCaoch();
+//		System.out.println(coach.getAdvise());
+//		
+//		coach = new TrackCoach();
+//		System.out.println(coach.getAdvise());
 	}
 }
